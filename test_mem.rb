f1645348a928dda2f58c801ce8f216e230f7a9fb
Iwo Dziechciarow <iiwo@o2.pl>
2021-01-13 16:19:02 +0100

    profile active job usage

--- test_mem.rb
+++ test_mem.rb
@@ -1,3 +1,5 @@
+require 'active_job'
+require 'active_job/queue_adapters/sidekiq_adapter'
 require 'sidekiq'
 require "sidekiq/pro/worker"
 require 'sidekiq/pro/api'
@@ -13,8 +15,18 @@ class AtomicJob
   sidekiq_options queue: 'test_queue', retry: 0
 
   def perform(account_id)
-    puts "RUNNING JOB: #{account_id}"
-    raise 'test error' if account_id.to_s == '3' # simulate error if needed
+    #puts "RUNNING JOB: #{account_id}"
+    #raise 'test error' if account_id.to_s == '3' # simulate error if needed
+  end
+end
+
+class AtomicJobAJ < ActiveJob::Base
+  self.queue_adapter = :sidekiq
+  queue_as 'test_queue'
+
+  def perform(account_id:)
+    #puts "RUNNING JOB: #{account_id}"
+    #raise 'test error' if account_id.to_s == '3' # simulate error if needed
   end
 end
 
@@ -39,6 +51,11 @@ class BatchJob
           account_id_batch = batch.map {|id| [id] }
           Sidekiq::Client.push_bulk('class' => AtomicJob, 'args' => account_id_batch)
         end
+      elsif ENV['INDIVIDUAL_AJ']
+        puts 'USING AJ INDIVIDUAL PUSH'
+        (1..NUMBER_OF_JOBS).each do |account_id|
+          AtomicJobAJ.perform_later(account_id: account_id)
+        end
       else
         puts 'USING INDIVIDUAL PUSH'
         (1..NUMBER_OF_JOBS).each do |account_id|
@@ -56,7 +73,7 @@ Sidekiq.logger.level = Logger::INFO # set log level
 if ENV['MEMORY_PROFILER']
   Sidekiq.configure_server do |config|
     config.server_middleware do |chain|
-      chain.add SidekiqProfilingMiddleware::MemoryProfiler, output_prefix: "sidekiq_mem_", only: [BatchJob].to_set
+      chain.add SidekiqProfilingMiddleware::MemoryProfiler, output_prefix: "aj_sidekiq_mem_", only: [BatchJob].to_set
     end
   end
 end
